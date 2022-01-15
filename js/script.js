f7085b3d37a97084de7b5e95a288a7c4f5f267f7
Samuel Otaviano <samuelotaviano@gmail.com>
2022-01-15 16:18:13 -0300

    1202 Parâmetros UR

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Aula 1201
+// Ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Aula 1202
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
