c92b12a2a5e1dfe58bbffc23f4bb47e84fad2ade
Samuel Otaviano <samuelotaviano@gmail.com>
2022-01-15 20:15:58 -0300

    1203 Perguntas Frequentes

--- js/script.js
+++ js/script.js
@@ -26,3 +26,35 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+// Aula 1203 (OBS.: Futuramente tentar por animação e fechar as perguntas que não foram clicadas para ficar só uma aberta)
+// Perguntas frequentes
+// Seleciona todos os botões
+const perguntas = document.querySelectorAll(".perguntas button");
+
+// Cria a função disparada pelo evento
+function ativarPergunta(event) {
+  // Identifica o botão que foi clicado
+  const pergunta = event.currentTarget;
+  // Pega o 'controle' do botão
+  const controls = pergunta.getAttribute("aria-controls");
+  // Pega a resposta por meio do Id
+  const resposta = document.getElementById(controls);
+
+  // Adiciona a classe à resposta
+  resposta.classList.toggle("ativa");
+  // Cria a const de ativa
+  const ativa = resposta.classList.contains("ativa");
+  // Troca o aria-expanded do botão para "true"
+  pergunta.setAttribute("aria-expanded", ativa); // vai ser "true" se existir e "false" se não existir
+}
+
+// Adiciona o event listener em cada pergunta
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+// Para cada item do conjunto de perguntas
+// aciona a a função que
+// insere o event listener (eventosPerguntas)
+perguntas.forEach(eventosPerguntas);
